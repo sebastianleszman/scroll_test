e1dd5347571144d1d59320c993c1b0f2b49f6aab
Sebastian Leszman <sebastianleszman@gmail.com>
2020-06-18 08:10:08 +0200

    another commit

--- main.js
+++ main.js
@@ -3,6 +3,12 @@ const innerHeight = document.querySelector('.innerHeight');
 const scrollHeight = document.querySelector('.scrollHeight');
 const scrollTop = document.querySelector('.scrollTop');
 
+const mainHeader = document.querySelector('.nav');
+
+let last = 0;
+let height = document.documentElement.scrollHeight;
+let client = document.documentElement.clientHeight;
+
 document.addEventListener('scroll', function () {
   let ch = document.documentElement.clientHeight;
   let ih = document.documentElement.innerHeght;
@@ -13,4 +19,15 @@ document.addEventListener('scroll', function () {
   innerHeight.innerHTML = `inner height ${ih}`;
   scrollHeight.innerHTML = `scroll height ${sh}`;
   scrollTop.innerHTML = `scroll top ${st}`;
+
+  let scroll = document.documentElement.scrollTop;
+
+  if (scroll > -1 || height - scroll > client - 20) {
+    if (scroll > last) {
+      mainHeader.classList.add('hide');
+    } else {
+      mainHeader.classList.remove('hide');
+    }
+    last = scroll;
+  }
 });
